f369757e6766fa1fdfd5f837878dcabcd87125d8
irinabottini <irina.bottini@bayer.com>
2026-06-30 08:53:17 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='campus-bayer-v1';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./assets/logo-bayer.jpg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
